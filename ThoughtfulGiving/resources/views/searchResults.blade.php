cee3c3f7d3dd9a4799d8ca0cb7dfd3131f729cb5
Kayla Martin <kaylaludmila@gmail.com>
2016-09-27 20:19:58 -0500

    messed with search blade

--- ThoughtfulGiving/resources/views/searchResults.blade.php
+++ ThoughtfulGiving/resources/views/searchResults.blade.php
@@ -2,26 +2,32 @@
 
 @section('content')
 <br>
-<div class="col l9 s12 white">
+<div class="col l9 s12">
 <!-- search results content  -->
-<table class="striped">
-  <thead>
-	    <tr>
-	      <th data-field="id" style="font-size:30px; color:#479ac4;">Search Results</th>
-	    </tr>
-	</thead>
-	<tbody>
-	    <tr>
-	        <td>Dog Kibble</td>
-	    </tr>
-	    <tr>
-	        <td>Leashes</td>
-	    </tr>
-	    <tr>
-	        <td>Paper Towels</td>
-	    </tr>
-	</tbody>
-</table>
+<div class="col l1 s12"></div>
+
+<div class="col l10 s12 white valign-wrapper">
+	<table class="striped">
+	  <thead>
+		    <tr>
+		      <th data-field="id" style="font-size:30px; color:#479ac4;">Search Results</th>
+		    </tr>
+		</thead>
+		<tbody>
+		    <tr>
+		        <td>Music Matters <a class="btn waves-effect tgPinkBG white-text darken-text-2 right-align"" href="/music">Learn More</a></td> 
+		    </tr>
+		    <tr>
+		        <td>Finding Fido</td>
+		    </tr>
+		    <tr>
+		        <td>Children Charity</td>
+		    </tr>
+		</tbody>
+	</table>
+</div>
+
+<div class="col l1 s12"></div>
 
 
 @endsection
