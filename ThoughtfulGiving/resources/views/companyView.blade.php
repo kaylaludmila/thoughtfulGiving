68c4d3e9ddfcb23fd172b6b6b1521cfbbe420bda
Kayla Martin <kaylaludmila@gmail.com>
2016-10-08 13:23:32 -0500

    updated layout of company template

--- ThoughtfulGiving/resources/views/companyView.blade.php
+++ ThoughtfulGiving/resources/views/companyView.blade.php
@@ -3,58 +3,57 @@
 @section('content')
 
 
- <div class="col l9 s12 white">
+<div class="col l9 s12 white">
 <br>
 
 <!-- Charity page content  -->
 @foreach ($company as $company)
 
-<img height="262" src={{ $company -> bannerURL }} alt="" class="responsive-img">
+<img height="262" alt="" class="responsive-img" src="{{ $company -> bannerURL }}">
 
       <div class= "row valign-wrapper">
-          <div class="col s12 m6 l2">
-            <img src={{ $company -> logoURL}} alt="" class="z-depth-1 responsive-img left-align">
-          </div>
-      <div class="col s12 m12 l9 valign">
-        <h1> {{ $company -> company }}</h1>
-        <p class="left-align s12 m12" style="font-size:17px;">Mission: {{ $company-> mission }}.</p>
+        <div class="col s12 m6 l2">
+            <img alt="" class="z-depth-1 responsive-img left-align" src="{{ $company -> logoURL}}">
+        </div>
+        <div class="col s12 m12 l9 valign">
+          <h1> {{ $company -> company }}</h1>
+          <p class="left-align s12 m12" style="font-size:17px;">Mission: {{ $company-> mission }}.</p>
         </div>
       </div>
 
-      <div class="row">
+  <div class="row">
 
-        <div class="col s12 l3">
-        </div>
+      <div class="col s12 l3">
+      </div>
 
         <div class="col s12 m4 l4">
          <table class="striped">
-        <thead>
-          <tr>
+          <thead>
+            <tr>
               <th data-field="id" style="font-size:30px; color:#479ac4;">In need of:</th>
-          </tr>
-        </thead>
-
-            <tbody>
-              @foreach ($items as $item)
-              <tr>
-                <td>
+            </tr>
+          </thead>
+              <tbody>
+                @foreach ($items as $item)
+                <tr>
+                  <td>
+                    
+                      {{ $item -> item }}
+                   
+                  </td>
                   
-                    {{ $item -> item }}
-                 
-                </td>
-                
-              </tr>
-                @endforeach
-            </tbody>
-      </table>
-      <br>
-      <a class="waves-effect waves-light btn" style="background-color:#ed4871;">Make a Monetary Donation </a>
-        </div>
+                </tr>
+                  @endforeach
+              </tbody>
+        </table>
+        <br>
+        <a class="waves-effect waves-light btn" style="background-color:#ed4871;">Make a Monetary Donation </a>
+      </div>
 
-        <div class="col s12 m1 l1">
-        </div>
+      <div class="col s12 m1 l1">
+      </div>
 
-        <div class="col s12 m4 l3">
+      <div class="col s12 m4 l3">
         <h5 style="color:#479ac4;"> Item drop off location</h5>
           <div class="card">
             <div class="card-image">
@@ -64,13 +63,11 @@
               <p>For More Info: <br> {{ $company -> firstName }}<br> {{ $company -> email }}</p>
             </div>
             <div class="card-action">
-              <a href={{ $company -> siteLink }} style="color:#479ac4 !important;">Official Website</a>
+              <a style="color:#479ac4 !important;" href="{{ $company -> siteLink }}">Official Website</a>
             </div>
         </div>
-     
     </div>
 
-</div>
+  </div>
 @endforeach
-</div>
 @endsection
