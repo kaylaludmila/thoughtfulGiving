fcc1590a447c411e0a41d7ac43ed4ab2c5d8a4ab
jess hust <jessicakhust@gmail.com>
2016-10-09 11:32:36 -0500

    companyView

--- ThoughtfulGiving/resources/views/companyView.blade.php
+++ ThoughtfulGiving/resources/views/companyView.blade.php
@@ -14,7 +14,9 @@
         <div class="col s12 m6 l2">
  </div>
         <div class="col s12 m12 l9 valign">
+        
           <h1> {{ $theCompany->company }}</h1>
+
           <p class="left-align s12 m12" style="font-size:17px;">Mission: {{ $theCompany-> mission }}.</p>
         </div>
       </div>
