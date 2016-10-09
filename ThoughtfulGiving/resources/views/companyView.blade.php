a909cc6edce629e9547cbdce5b84504e0126409a
Kayla Martin <kaylaludmila@gmail.com>
2016-10-09 13:04:21 -0500

    updating link

--- ThoughtfulGiving/resources/views/companyView.blade.php
+++ ThoughtfulGiving/resources/views/companyView.blade.php
@@ -59,7 +59,7 @@
               <p>For More Info: <br> {{ $theCompany -> firstName }}<br> {{ $theCompany -> email }}</p>
             </div>
             <div class="card-action">
-              <a style="color:#479ac4 !important;" href="{{ HTML::link ($theCompany -> siteLink) }}" target="_blank">Official Website</a>
+              {{ HTML::link ("$theCompany -> siteLink", "Official Website") }}
             </div>
         </div>
     </div>
